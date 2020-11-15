fb679c0ee03366e25aa21ed4a7af6844afc8f511
ajay7777 <singhajay1194@gmail.com>
2020-11-15 14:48:42 +0100

    added intitial comparators and blockers

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Blocking/RestaurantBlockingKeyByZipCode.java
@@ -0,0 +1,21 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Blocking;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Restaurant;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.generators.RecordBlockingKeyGenerator;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.Pair;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.DataIterator;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+public class RestaurantBlockingKeyByZipCode extends
+        RecordBlockingKeyGenerator<Restaurant, Attribute> {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void generateBlockingKeys(Restaurant record, Processable<Correspondence<Attribute, Matchable>> correspondences, DataIterator<Pair<String, Restaurant>> resultCollector) {
+        resultCollector.next(new Pair<>(Integer.toString(record.getZip()), record));
+    }
+}
